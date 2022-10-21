e81c908c4d84fbb5dfd8cb225f4c721758725fd0
Ganfeng0668 <1149670811@qq.com>
2022-10-21 22:13:19 +0800

    first

--- src/main/java/com/gan/demo/config/MyInterceptorConfig.java
+++ src/main/java/com/gan/demo/config/MyInterceptorConfig.java
@@ -1,2 +1,11 @@
-package com.gan.demo.config;public class MyInterceptorConfig {
+package com.gan.demo.config;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MyInterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
 }
--- src/main/java/com/gan/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gan/demo/exception/GlobalExceptionHandler.java
@@ -1,2 +1,14 @@
-package com.gan.demo.exception;public class GlobalExceptionHandler {
+package com.gan.demo.exception;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 }
